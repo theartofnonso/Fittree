5f8ee6def48012da46c73220809620182615dc96
Africanfruit <slimbion@gmail.com>
2022-08-15 10:27:26 +0100

    Update Socials.js

--- src/components/views/Socials.js
+++ src/components/views/Socials.js
@@ -15,21 +15,21 @@ const Socials = ({profile}) => {
             marginVertical: 20
         }}>
             {profile.instagram ?
-                <TouchableOpacity style={{marginHorizontal: 6}}>
+                <TouchableOpacity style={{marginHorizontal: 8}}>
                     <Link target="_blank" href={INSTAGRAM + profile.instagram} sx={{textDecoration: 'none'}}
                           rel="noopener">
                         <SimpleLineIcons name="social-instagram" size={24} color="#ef7a75"/>
                     </Link>
                 </TouchableOpacity> : null}
             {profile.facebook ?
-                <TouchableOpacity style={{marginHorizontal: 6}}>
+                <TouchableOpacity style={{marginHorizontal: 8}}>
                     <Link target="_blank" href={INSTAGRAM + profile.facebook} sx={{textDecoration: 'none'}}
                           rel="noopener">
                         <SimpleLineIcons name="social-facebook" size={24} color="#ef7a75"/>
                     </Link>
                 </TouchableOpacity> : null}
             {profile.twitter ?
-                <TouchableOpacity style={{marginHorizontal: 6}}>
+                <TouchableOpacity style={{marginHorizontal: 8}}>
                     <Link target="_blank" href={TWITTER + profile.twitter} sx={{textDecoration: 'none'}}
                           rel="noopener">
                         <SimpleLineIcons name="social-twitter" size={24} color="#ef7a75"/>
@@ -39,14 +39,14 @@ const Socials = ({profile}) => {
             {/*    <TouchableOpacity style={{marginHorizontal: 6}}><SimpleLineIcons name="social-spotify" size={24}*/}
             {/*                                                                     color="#ef7a75"/></TouchableOpacity> : null}*/}
             {profile.tiktok ?
-                <TouchableOpacity style={{marginLeft: 1, marginRight: 6}}>
+                <TouchableOpacity style={{marginHorizontal: 8}}>
                     <Link target="_blank" href={TIKTOK + profile.tiktok} sx={{textDecoration: 'none'}}
                           rel="noopener">
                         <TikTokSvg/>
                     </Link>
                 </TouchableOpacity> : null}
             {profile.youtube ?
-                <TouchableOpacity style={{marginHorizontal: 6}}>
+                <TouchableOpacity style={{marginHorizontal: 8}}>
                     <Link target="_blank" href={YOUTUBE + profile.youtube} sx={{textDecoration: 'none'}}
                           rel="noopener">
                         <SimpleLineIcons name="social-youtube" size={24} color="#ef7a75"/>
