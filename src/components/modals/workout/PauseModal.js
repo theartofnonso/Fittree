4c4e673d6a2d331c09ae95ca58f73444a66c778d
Africanfruit <slimbion@gmail.com>
2022-08-30 18:51:18 +0100

    Update PauseModal.js

--- src/components/modals/workout/PauseModal.js
+++ src/components/modals/workout/PauseModal.js
@@ -1,23 +1,35 @@
 /* eslint-disable */
 import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native-web";
-import Entypo from "react-native-vector-icons/Entypo";
-import {Modal} from "react-native-paper";
+import {StyleSheet, TouchableOpacity, View} from "react-native-web";
+import {Entypo} from "@expo/vector-icons";
+import {Typography} from "@mui/material";
 
 const PauseModal = props => {
 
-  return (
-      <Modal style={styles.root} visible={props.isVisible}>
-          <View style={styles.container}>
-            <TouchableOpacity style={styles.closeBtn} onPress={props.close}>
-              <Entypo name="cross" size={32} color="white"/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={props.play}>
-              <Entypo name="controller-play" size={50} color="white" />
-            </TouchableOpacity>
-          </View>
-      </Modal>
-  );
+    return (
+        <View style={styles.root}>
+            <View style={styles.container}>
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    style={styles.skipBtn}
+                    onPress={props.play}
+                    testID="End_Workout_Btn">
+                    <Entypo name="controller-play" size={48} color="white"/>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    style={styles.endWorkoutBtn}
+                    onPress={props.navigateToWorkoutPreview}
+                    testID="End_Workout_Btn">
+                    <Typography style={{color: 'white', fontFamily: 'Montserrat', fontWeight: 'bold'}}>
+                        End Workout
+                    </Typography>
+                </TouchableOpacity>
+
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -37,10 +49,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  closeBtn: {
-    position: 'fixed',
-    top: 10,
-    right: 10,
+  endWorkoutBtn: {
+    alignItems: 'center',
+    backgroundColor: '#ef7a75',
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: 200,
+    height: 40,
+    position: 'absolute',
+    bottom: 50,
   },
 });
 
